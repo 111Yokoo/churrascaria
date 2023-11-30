0268c46f330bc7748e85f629bb9497f160e2c208
111Yokoo <yokoomatheus@gmail.com>
2023-11-30 08:35:29 -0300

    Atualização final

--- churrasco/Public/Midias/Js/js.js
+++ churrasco/Public/Midias/Js/js.js
@@ -9,9 +9,9 @@ $(document).ready(() => {
        
        fotos.append(imagem1, imagem2); // Adiciona o novo elemento img à div fotos \\
 
-       $("#btn-carnes").click(function () {
+    $("#btn-carnes").click(function () {
             window.location.href = '/carnes'; 
-        });
+    });
     });
     $("#btn-carnes").mouseleave(function () {
         var fotos = $('#fotos');
@@ -27,7 +27,7 @@ $(document).ready(() => {
        fotos.append(imagem1, imagem2);
 
        $("#btn-adic").click(function () {
-            window.location.href = '/acompanhamentos'; 
+            window.location.href = '/adicionais'; 
         });
     });
     $("#btn-adic").mouseleave(function () {
@@ -78,7 +78,7 @@ $(document).ready(() => {
        fotos.append(imagem1, imagem2);
 
        $("#btn-sobremesa").click(function () {
-            window.location.href = '/doces'; 
+            window.location.href = '/sobremesas'; 
         });
     });
     $("#btn-sobremesa").mouseleave(function () {
@@ -86,5 +86,298 @@ $(document).ready(() => {
        fotos.find('img').remove();
     });
 
+///Pedidos///
+    var preco = 0;
+    var preco_carnes = 0;
+    var preco_adicionais = 0;
+    var preco_salada = 0;
+    var preco_bebidas = 0;
+    var preco_sobremesas = 0;
+    var pedido = {'cliente' : [],'carnes' : [], 'adicionais' : [], 'salada' : [], 'bebidas' : [], 'sobremesas' : [], 'preco': []};
+
+    let idCounter = 0; 
+   
+$('#carneEscolha').on('change', function() {
+        let selectedValues = $(this).find('option:selected').map(function() {
+            return $(this).val();
+        }).get();
     
+        var selectedValuesString = selectedValues.join(', ');
+    
+        var pedidoEscolha = selectedValuesString.split("/");
+    
+        if(pedidoEscolha[0] != ''){
+            idCounter++;
+        
+            for (let i = 0; i < selectedValues.length; i++) {
+                $('#carneContainer').append(`
+                    <p data-id="${idCounter}">
+                        <span>${pedidoEscolha[0]}</span>
+                        R$${pedidoEscolha[1]}
+                        <button class="btnX"><i class="fa-solid fa-xmark"></i></button>
+                    </p>
+                `);
+
+                var precoEscolhaAtual = Number(pedidoEscolha[1]);
+
+                pedido['carnes'].push({
+                    id: idCounter,
+                    item: pedidoEscolha[0],
+                    preco: precoEscolhaAtual
+                });
+
+                preco_carnes = preco_carnes + precoEscolhaAtual;
+                printarPreco('carnes');
+            }
+        }
+});
+$('#adicionaisEscolha').on('change', function() {
+    let selectedValues = $(this).find('option:selected').map(function() {
+        return $(this).val();
+    }).get();
+
+    var selectedValuesString = selectedValues.join(', ');
+
+    var pedidoEscolha = selectedValuesString.split("/");
+
+    if(pedidoEscolha[0] != ''){
+        idCounter++;
+    
+        for (let i = 0; i < selectedValues.length; i++) {
+            $('#adicionaisContainer').append(`
+                <p data-id="${idCounter}">
+                    <span>${pedidoEscolha[0]}</span>
+                    R$${pedidoEscolha[1]}
+                    <button><i class="fa-solid fa-xmark"></i></button>
+                </p>
+            `);
+        
+            var precoEscolhaAtual = Number(pedidoEscolha[1]);
+
+            pedido['adicionais'].push({
+                id: idCounter,
+                item: pedidoEscolha[0],
+                preco: precoEscolhaAtual
+            });
+
+            preco_adicionais = preco_adicionais + precoEscolhaAtual;
+            printarPreco('adicionais');
+        }
+    }
+});
+$('#saladaEscolha').on('change', function() {
+    let selectedValues = $(this).find('option:selected').map(function() {
+        return $(this).val();
+    }).get();
+
+    var selectedValuesString = selectedValues.join(', ');
+
+    var pedidoEscolha = selectedValuesString.split("/");
+
+    if(pedidoEscolha[0] != ''){
+        idCounter++;
+    
+        for (let i = 0; i < selectedValues.length; i++) {
+            $('#saladaContainer').append(`
+                <p data-id="${idCounter}">
+                    <span>${pedidoEscolha[0]}</span>
+                    R$${pedidoEscolha[1]}
+                    <button><i class="fa-solid fa-xmark"></i></button>
+                </p>
+            `);
+        
+            var precoEscolhaAtual = Number(pedidoEscolha[1]);
+            
+            pedido['salada'].push({
+                id: idCounter,
+                item: pedidoEscolha[0],
+                preco: precoEscolhaAtual
+            });
+
+            preco_salada = preco_salada + precoEscolhaAtual;
+            printarPreco('salada');
+        }
+    }
+});
+$('#bebidasEscolha').on('change', function() {
+    let selectedValues = $(this).find('option:selected').map(function() {
+        return $(this).val();
+    }).get();
+
+    var selectedValuesString = selectedValues.join(', ');
+
+    var pedidoEscolha = selectedValuesString.split("/");
+
+    if(pedidoEscolha[0] != ''){
+        idCounter++;
+    
+        for (let i = 0; i < selectedValues.length; i++) {
+            $('#bebidasContainer').append(`
+                <p data-id="${idCounter}">
+                    <span>${pedidoEscolha[0]}</span>
+                    R$${pedidoEscolha[1]}
+                    <button><i class="fa-solid fa-xmark"></i></button>
+                </p>
+            `);
+        
+            var precoEscolhaAtual = Number(pedidoEscolha[1]);
+            
+            pedido['bebidas'].push({
+                id: idCounter,
+                item: pedidoEscolha[0],
+                preco: precoEscolhaAtual
+            });
+
+            preco_bebidas = preco_bebidas + precoEscolhaAtual;
+            printarPreco('bebidas');
+        }
+    }
+});
+$('#sobremesasEscolha').on('change', function() {
+    let selectedValues = $(this).find('option:selected').map(function() {
+        return $(this).val();
+    }).get();
+   
+    var selectedValuesString = selectedValues.join(', ');
+
+    var pedidoEscolha = selectedValuesString.split("/");
+
+    if(pedidoEscolha[0] != ''){
+        idCounter++;
+    
+        for (let i = 0; i < selectedValues.length; i++) {
+            $('#sobremesasContainer').append(`
+                <p data-id="${idCounter}">
+                    <span>${pedidoEscolha[0]}</span>
+                    R$${pedidoEscolha[1]}
+                    <button><i class="fa-solid fa-xmark"></i></button>
+                </p>
+            `);
+        
+            var precoEscolhaAtual = Number(pedidoEscolha[1]);
+            
+            pedido['sobremesas'].push({
+                id: idCounter,
+                item: pedidoEscolha[0],
+                preco: precoEscolhaAtual
+            });
+
+            preco_sobremesas = preco_sobremesas + precoEscolhaAtual;
+            printarPreco('sobremesas');
+        }
+    }
+});
+function obterValoresDoFormulario() {
+    var nome = document.getElementById('nome').value;
+    var endereco = document.getElementById('endereco').value;
+    var numero = document.getElementById('numero').value;
+    var complemento = document.getElementById('complemento').value;
+    var telefone = document.getElementById('telefone').value;
+
+    if (nome === '' || endereco === '' || numero === '' || complemento === '' || telefone === '') {
+        alert('Por favor, preencha todos os campos do cartão "Dados para entrega".');
+        return false;
+    }
+
+    pedido['cliente'].push({
+        nome: nome,
+        endereco: endereco,
+        numero: numero,
+        complemento: complemento,
+        telefone: telefone
+    });
+
+    return true;
+}    
+$('#escolhidos').on('click', 'button', function() {
+        var idToRemove = $(this).closest('p').data('id');
+    
+        $(this).closest('p').remove();
+    
+        // Removendo o elemento correspondente do array na variavel pedido
+        var encontrado = false;
+        for (var tipoProduto in pedido) { //Passar pela var pedido e colocar os [values] dentro de tipoProduto
+            pedido[tipoProduto] = pedido[tipoProduto].filter(function(item) { //Passar pelos elementos que estão dentro de pedido[tipoProduto] e colocar os elementos na variavel item
+                if (item.id === idToRemove) { //Se o item.id for identico ai id que foi pego do objeto removido ent{remove item retornando false}
+                    encontrado = true;
+                    return false;
+                }
+                return true; // Mantém os itens no array
+            });
+
+            // Se encontrou o item em um tipo de produto, atualiza o preço e sai do loop
+            if (encontrado) {
+                printarPreco(tipoProduto);
+                break;
+            }
+        }
+    
+        // Se não encontrou o item em nenhum tipo de produto, redefine o preço para 0
+        if (!encontrado) {
+            preco = 0;
+            $('#precoEscolhidos').html('R$' + preco + '.00');
+        }
+});
+function printarPreco(tipoProduto) {
+    var novoPreco = pedido[tipoProduto].reduce(function(total, item) {
+        return total + item.preco;
+    }, 0);
+
+    switch(tipoProduto){
+        case 'carnes':
+            preco_carnes = novoPreco;
+            break;
+        case 'adicionais':
+            preco_adicionais = novoPreco;
+            break;
+        case 'salada':
+            preco_salada = novoPreco;
+            break;
+        case 'bebidas':
+            preco_bebidas = novoPreco;
+            break;
+        case 'sobremesas':
+            preco_sobremesas = novoPreco;
+            break;
+        default:
+            console.log("Tipo inválido");
+            break;
+    }
+
+    preco = preco_carnes + preco_adicionais + preco_salada + preco_bebidas + preco_sobremesas;
+
+    
+    // Atualiza o elemento HTML para mostrar o novo preço total
+    $('#precoEscolhidos').html('R$' + preco + '.00');
+}
+//Mandando dados para o php IndexController 
+$('#btnFazerPedido').on('click', function() {
+        if (!obterValoresDoFormulario()) {
+            return;
+        }
+
+        pedido['preco'].push({
+            'preco_total': preco
+        });
+
+        var dadosParaEnviar = JSON.stringify(pedido);
+
+        $.ajax({
+            type: 'POST',
+            url: '/fazerPedido',
+            data: { pedido: dadosParaEnviar },
+            dataType: 'json',
+            success: function (dados) {
+                if (dados.success) {
+                    alert(dados.resultadoPositivo);
+                    location.reload();
+                } else {
+                    alert('Erro ao fazer o pedido, tente novamente');
+                }
+            },
+            error: function (erro) {
+                console.error('Erro ao enviar dados para o PHP:', erro);
+            }
+        });
+});
 });
--- churrasco/vendor/composer/autoload_static.php
+++ churrasco/vendor/composer/autoload_static.php
@@ -9,6 +9,7 @@ class ComposerStaticInit088cc0dd4532f46de4896f441c95ec1c
     public static $files = array (
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -18,6 +19,7 @@ class ComposerStaticInit088cc0dd4532f46de4896f441c95ec1c
         ),
         'S' => 
         array (
+            'Symfony\\Polyfill\\Php80\\' => 23,
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Ctype\\' => 23,
         ),
@@ -35,6 +37,10 @@ class ComposerStaticInit088cc0dd4532f46de4896f441c95ec1c
         'Twig\\' => 
         array (
             0 => __DIR__ . '/..' . '/twig/twig/src',
+        ),
+        'Symfony\\Polyfill\\Php80\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-php80',
         ),
         'Symfony\\Polyfill\\Mbstring\\' => 
         array (
@@ -55,7 +61,12 @@ class ComposerStaticInit088cc0dd4532f46de4896f441c95ec1c
     );
 
     public static $classMap = array (
+        'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+        'PhpToken' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/PhpToken.php',
+        'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
+        'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
+        'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
